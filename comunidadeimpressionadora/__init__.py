7da014dff44a345ede87f08290ae4c1045855c41
Luissantos20 <luissilvassantos@gmail.com>
2025-08-14 14:33:44 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -7,8 +7,8 @@ import os
 # App
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1e34128f52ed4465e4be4734ed6b969d'
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
