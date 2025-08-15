add70aad610041c824412b8c2493557cca6b3c5b
Luissantos20 <luissilvassantos@gmail.com>
2025-08-15 11:32:42 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,32 +4,33 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 import sqlalchemy
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # App
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = '1e34128f52ed4465e4be4734ed6b969d'
+
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
-#Banco de dados
+# Banco de dados
 database = SQLAlchemy(app)
 
-#Criptografia das senhas
+# Criptografia das senhas
 bcrypt = Bcrypt(app)
 
-#Login Manager
+# Login Manager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import models
+
+# Verifica se a tabela existe
 engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
+
 if not inspector.has_table("usuario"):
     with app.app_context():
         database.drop_all()
@@ -38,6 +39,5 @@ if not inspector.has_table("usuario"):
 else:
     print("Base de dados já existente")
 
-#Routes
+# Routes
 from comunidadeimpressionadora import routes
-
